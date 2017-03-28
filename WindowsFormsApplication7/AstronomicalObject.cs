5c9b508cab8f3585f9f2f4340badef2b95135484
topProgrammist <lesha.skachcov98@gmail.com>
2017-03-28 15:36:29 +0300

    test
    test

---
+++ WindowsFormsApplication7/AstronomicalObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication7
+{
+    [Serializable]
+    public abstract class AstronomicalObject
+    {
+        public string Name { get; set; }
+        public double Mass { get; set; }
+        public double Radius { get; set; }
+        public double Temperature { get; set; }
+        public double RotationPeriod { get; set; }
+        public double DistanceToTheStar { get; set; }
+    }
+}
